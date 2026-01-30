7d7ce297060a3866cce067e44270358234ccdfcd
i am Chuppyy <62337417+chuppyy@users.noreply.github.com>
2026-01-30 14:14:00 +0700

    Update [slug].tsx

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -206,11 +206,11 @@ export async function getStaticProps({ params }: { params: any }) {
     const parameters = {
       videoScriptSrc: "https://nexvelar.digital/ads/nthotnews_boonovel_com.eb373146-0084-4675-83c9-23556caad088.video.js",
       //Code auto 
-      googleClientId: "ca-pub-2388584177550957",
+      googleClientId: "ca-pub-7472198107183412",
       //GA tiêu đề
-      googleClientSlotId:"1402496095",
+      googleClientSlotId:"2746940340",
       //GA sau video
-      googleAdSlot: "3457265057",
+      googleAdSlot: "6686185351",
 //Cái sau
       mgWidgetId1: "1871059",
       //Cái trước
